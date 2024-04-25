ad9ba9ad0174f9ac915cf93556f440cafeefdd3f
DBrayanG <72024948+DBrayanG@users.noreply.github.com>
2024-04-25 19:39:26 -0400

    prueba de http

--- resources/views/pizarra/index.blade.php
+++ resources/views/pizarra/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Diagrama de Secuencia</title>
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
     <!-- Agrega el enlace a los archivos CSS de Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
